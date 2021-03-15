1d3fd1dd8c8fb75b44af352809d10f5e3d7253e9
ODoctorDu <odoctordu@gmail.com>
2021-03-15 15:58:03 -0300

    First Controller and razor pages test

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "My first MVC Program.";
+            ViewData["Developer Name"] = "Eduardo Souza";
 
             return View();
         }
